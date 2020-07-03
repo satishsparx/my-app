f43fd95939794202f6a1203d2935188d2bcbfa02
satish <satish.kallepalli@cerner.com>
2020-07-03 14:56:06 +0530

    commenting service.js

--- src/service.js
+++ src/service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Api call for all the places data
 const getPlaces = async() =>{
     try {
         const resp = await axios.get('https://roadtrippers.herokuapp.com/api/v1/places');
@@ -9,6 +10,7 @@ const getPlaces = async() =>{
     }
 }
 
+// Api call for the specific place
 const getPlaceInfo = async(id) => {
     try {
         const resp = await axios.get('http://roadtrippers.herokuapp.com/api/v1/place/'+ id.toString());
